d2216fe8721740b14abbd80d56ea37c1c4e7533a
Gabe <gabriel.velasquez0116@gmail.com>
2023-03-12 15:56:38 -0600

    setting up js

---
+++ script.js
@@ -0,0 +1,20 @@
+var database = [
+    {
+        username: "Gabe",
+        password: "MeowMix4Evah"
+    }
+];
+
+var newsFeed = [
+    {
+        username: "Hannah",
+        timeline: "What's for dinner?"
+    },
+    {
+        username: "Stella",
+        timeline: "What time is dinner?"
+    }
+];
+
+var userNamePrompt = prompt("What's your username?");
+var passwordPrompt = prompt("What's your password?");
