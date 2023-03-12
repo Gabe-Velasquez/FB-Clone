b04e24d34910a52c713372fa62069233502cff32
Gabe <gabriel.velasquez0116@gmail.com>
2023-03-12 16:33:36 -0600

    created login credential identifiers

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ var database = [
     {
         username: "Gabe",
         password: "MeowMix4Evah"
+    },
+    {
+        username: "Dexter",
+        password: "Bark123"
+    },
+    {
+        username: "Morg",
+        password: "Poop2022"
     }
 ];
 
@@ -16,5 +24,26 @@ var newsFeed = [
     }
 ];
 
+function isUserValid(username, password){
+    For (var i=0; i <database.length; i++){
+        if (database[i].username === username && database[i].password === password){
+            return true;
+        } else {
+            alert("Sorry, wrong username and/or password. Please try again.");
+        }
+    }
+    return false;
+}
+
+function signIn(user, pass){
+    if (isUserValid(username, password)) {
+        console.log(newsfeed);
+    } else {
+        alert("Sorry, wrong username and/or password. Please try again. ")
+    }
+    }
+
 var userNamePrompt = prompt("What's your username?");
 var passwordPrompt = prompt("What's your password?");
+
+signIn(userNamePrompt, passwordPrompt);
