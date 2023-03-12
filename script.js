0a6ce7f43b4250c99ac6cabd7662872fe8cdf875
Gabe <gabriel.velasquez0116@gmail.com>
2023-03-12 16:36:32 -0600

    prettier formatted

--- script.js
+++ script.js
@@ -1,47 +1,50 @@
 var database = [
-    {
-        username: "Gabe",
-        password: "MeowMix4Evah"
-    },
-    {
-        username: "Dexter",
-        password: "Bark123"
-    },
-    {
-        username: "Morg",
-        password: "Poop2022"
-    }
+  {
+    username: "Gabe",
+    password: "MeowMix4Evah",
+  },
+  {
+    username: "Dexter",
+    password: "Bark123",
+  },
+  {
+    username: "Morg",
+    password: "Poop2022",
+  },
 ];
 
 var newsFeed = [
-    {
-        username: "Hannah",
-        timeline: "What's for dinner?"
-    },
-    {
-        username: "Stella",
-        timeline: "What time is dinner?"
-    }
+  {
+    username: "Hannah",
+    timeline: "What's for dinner?",
+  },
+  {
+    username: "Stella",
+    timeline: "What time is dinner?",
+  },
 ];
 
-function isUserValid(username, password){
-    For (var i=0; i <database.length; i++){
-        if (database[i].username === username && database[i].password === password){
-            return true;
-        } else {
-            alert("Sorry, wrong username and/or password. Please try again.");
-        }
+function isUserValid(username, password) {
+  for (var i = 0; i < database.length; i++) {
+    if (
+      database[i].username === username &&
+      database[i].password === password
+    ) {
+      return true;
+    } else {
+      alert("Sorry, wrong username and/or password. Please try again.");
     }
-    return false;
+  }
+  return false;
 }
 
-function signIn(user, pass){
-    if (isUserValid(username, password)) {
-        console.log(newsfeed);
-    } else {
-        alert("Sorry, wrong username and/or password. Please try again. ")
-    }
-    }
+function signIn(user, pass) {
+  if (isUserValid(username, password)) {
+    console.log(newsfeed);
+  } else {
+    alert("Sorry, wrong username and/or password. Please try again. ");
+  }
+}
 
 var userNamePrompt = prompt("What's your username?");
 var passwordPrompt = prompt("What's your password?");
